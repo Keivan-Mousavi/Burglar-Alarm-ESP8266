022fb919710508f535dec7610b5a2f5165fd063d
keivan mousavi <keivan_mousavi@hotmail.com>
2021-01-28 00:56:45 +0330

    clear code

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,17 @@
 #include <IRsend.h>
 #include <ArduinoJson.h>
 
-const char *ssid = "Keivan";
-const char *password = "K@0e#5I%8v^3A*7n?";
-const uint16_t kIrLed = 4; // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+const char *ssid = "***";
+const char *password = "***";
 
-IRsend irsend(kIrLed); // Set the GPIO to be used to sending the message.
+const uint16_t kIrLed = 4;
+IRsend irsend(kIrLed);
+
+int sound_digital = 4;
+int sound_analog = 0;
 
 void setup()
 {
-  irsend.begin();
   //#if ESP8266
   //   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
   // #else  // ESP8266
@@ -21,11 +23,16 @@ void setup()
   // #endif // ESP8266
 
   Serial.begin(115200);
+
+  pinMode(sound_digital, INPUT);
+  pinMode(sound_digital, INPUT);
+
+  irsend.begin();
+
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED)
   {
-
     delay(1000);
     Serial.print("Connecting..");
   }
@@ -33,39 +40,76 @@ void setup()
 
 void loop()
 {
+  int val_digital = digitalRead(sound_digital);
+  int val_analog = analogRead(sound_analog);
 
-  Serial.println("NEC");
-  irsend.sendNEC(0x20DF10EF);
-  delay(2000);
+  if (val_analog > 500)
+  {
+    irsend.sendNEC(0x20DF10EF);
 
-  if (WiFi.status() == WL_CONNECTED)
-  { //Check WiFi connection status
+    Serial.println(val_analog);
+    Serial.println(val_digital);
 
-    HTTPClient http; //Declare an object of class HTTPClient
+    if (WiFi.status() == WL_CONNECTED)
+    {
 
-    http.begin("http://jsonplaceholder.typicode.com/users/1"); //Specify request destination
-    int httpCode = http.GET();                                 //Send the request
+      HTTPClient http; //Declare an object of class HTTPClient
 
-    if (httpCode > 0)
-    { //Check the returning code
-      Serial.println("http : ");
-      String payload = http.getString(); //Get the request response payload
+      http.begin("http://jsonplaceholder.typicode.com/users/1");
+      int httpCode = http.GET();
 
-      DynamicJsonDocument doc(1024);
-      deserializeJson(doc, payload);
+      if (httpCode > 0)
+      {
+        Serial.println("http : ");
+        String payload = http.getString();
 
-      JsonObject json = doc.as<JsonObject>();
+        DynamicJsonDocument doc(1024);
+        deserializeJson(doc, payload);
 
-      String phone = json[String("phone")];
+        JsonObject json = doc.as<JsonObject>();
 
-      Serial.println(phone);
+        String phone = json[String("phone")];
 
+        Serial.println(phone);
 
-     // Serial.println(payload);           //Print the response payload
+        // Serial.println(payload);           //Print the response payload
+      }
+
+      http.end(); //Close connection
     }
-
-    http.end(); //Close connection
   }
 
-   delay(2000); //Send a request every 30 seconds
+  // Serial.println("NEC");
+  // irsend.sendNEC(0x20DF10EF);
+  // delay(2000);
+
+  // if (WiFi.status() == WL_CONNECTED)
+  // { //Check WiFi connection status
+
+  //   HTTPClient http; //Declare an object of class HTTPClient
+
+  //   http.begin("http://jsonplaceholder.typicode.com/users/1"); //Specify request destination
+  //   int httpCode = http.GET();                                 //Send the request
+
+  //   if (httpCode > 0)
+  //   { //Check the returning code
+  //     Serial.println("http : ");
+  //     String payload = http.getString(); //Get the request response payload
+
+  //     DynamicJsonDocument doc(1024);
+  //     deserializeJson(doc, payload);
+
+  //     JsonObject json = doc.as<JsonObject>();
+
+  //     String phone = json[String("phone")];
+
+  //     Serial.println(phone);
+
+  //     // Serial.println(payload);           //Print the response payload
+  //   }
+
+  //   http.end(); //Close connection
+  // }
+
+  // delay(2000); //Send a request every 30 seconds
 }
