75628a84e2878fd6151c463a537f6ddec281614f
keivan mousavi <keivan_mousavi@hotmail.com>
2021-01-30 23:29:52 +0330

    Sound Check Inviroment

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,10 @@ IRsend irsend(kIrLed);
 int sound_digital = 4;
 int sound_analog = 0;
 
+bool flag = false;
+
 void setup()
 {
-  //#if ESP8266
-  //   Serial.begin(115200, SERIAL_8N1, SERIAL_TX_ONLY);
-  // #else  // ESP8266
-  //   Serial.begin(115200, SERIAL_8N1);
-  // #endif // ESP8266
-
   Serial.begin(115200);
 
   pinMode(sound_digital, INPUT);
@@ -34,82 +30,76 @@ void setup()
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(1000);
-    Serial.print("Connecting..");
+    Serial.print("Connecting.." + WiFi.status());
   }
 }
 
-void loop()
+void SoundCheckInviroment()
 {
   int val_digital = digitalRead(sound_digital);
   int val_analog = analogRead(sound_analog);
 
   if (val_analog > 500)
   {
-    irsend.sendNEC(0x20DF10EF);
-
     Serial.println(val_analog);
-    Serial.println(val_digital);
 
-    if (WiFi.status() == WL_CONNECTED)
+    HTTPClient http;
+
+    String url = "http://burglaralarm.persianprogrammer.com/ManageNotification/CheckCamera?serial=d6ac5b88-35e9-461f-b911-2f68d4cb9c44";
+    http.begin(url);
+
+    int httpCode = http.GET();
+
+    if (httpCode > 0)
     {
+      flag = (bool)http.getString();
 
-      HTTPClient http; //Declare an object of class HTTPClient
-
-      http.begin("http://jsonplaceholder.typicode.com/users/1");
-      int httpCode = http.GET();
-
-      if (httpCode > 0)
+      if (flag == true)
       {
-        Serial.println("http : ");
-        String payload = http.getString();
-
-        DynamicJsonDocument doc(1024);
-        deserializeJson(doc, payload);
-
-        JsonObject json = doc.as<JsonObject>();
-
-        String phone = json[String("phone")];
-
-        Serial.println(phone);
-
-        // Serial.println(payload);           //Print the response payload
+        irsend.sendNEC(0x20DF10EF);
       }
+    }
+    http.end();
+  }
+}
 
-      http.end(); //Close connection
+void ControllerTV()
+{
+  int val_digital = digitalRead(sound_digital);
+  int val_analog = analogRead(sound_analog);
+
+  if (val_analog > 500)
+  {
+    Serial.println(val_analog);
+
+    HTTPClient http;
+
+    String url = "http://burglaralarm.persianprogrammer.com/ManageNotification/CheckCamera?serial=d6ac5b88-35e9-461f-b911-2f68d4cb9c44";
+    http.begin(url);
+
+    int httpCode = http.GET();
+
+    if (httpCode > 0)
+    {
+      flag = (bool)http.getString();
+
+      if (flag == true)
+      {
+        irsend.sendNEC(0x20DF10EF);
+      }
+    }
+    http.end();
+  }
+}
+
+void loop()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    SoundCheckInviroment();
+
+    if (flag == true)
+    {
     }
   }
-
-  // Serial.println("NEC");
-  // irsend.sendNEC(0x20DF10EF);
-  // delay(2000);
-
-  // if (WiFi.status() == WL_CONNECTED)
-  // { //Check WiFi connection status
-
-  //   HTTPClient http; //Declare an object of class HTTPClient
-
-  //   http.begin("http://jsonplaceholder.typicode.com/users/1"); //Specify request destination
-  //   int httpCode = http.GET();                                 //Send the request
-
-  //   if (httpCode > 0)
-  //   { //Check the returning code
-  //     Serial.println("http : ");
-  //     String payload = http.getString(); //Get the request response payload
-
-  //     DynamicJsonDocument doc(1024);
-  //     deserializeJson(doc, payload);
-
-  //     JsonObject json = doc.as<JsonObject>();
-
-  //     String phone = json[String("phone")];
-
-  //     Serial.println(phone);
-
-  //     // Serial.println(payload);           //Print the response payload
-  //   }
-
-  //   http.end(); //Close connection
-  // }
-
-  // delay(2000); //Send a request every 30 seconds
 }
